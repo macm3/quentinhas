95558cf44cdd1f730046ce9706051afb13b65505
lmmc2 <lmmc2@cin.ufpe.br>
2018-11-23 19:09:43 -0300

    Splash screen e layout login

--- App/app/src/main/java/com/ufpe/if710/quentinhas/MainActivity.kt
+++ App/app/src/main/java/com/ufpe/if710/quentinhas/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.ufpe.if710.quentinhas
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val handle = Handler()
+        handle.postDelayed({goToLogin()}, 2000)
     }
+
+    private fun goToLogin(){
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
 }
