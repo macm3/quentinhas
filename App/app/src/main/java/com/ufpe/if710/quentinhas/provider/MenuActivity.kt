4ec24c1acf3ecb7b4a1559347044c4129a9aa0f6
lmmc2 <lmmc2@cin.ufpe.br>
2018-12-09 16:25:28 -0300

    Detalhes do cardápio com dados do firebase

--- App/app/src/main/java/com/ufpe/if710/quentinhas/provider/MenuActivity.kt
+++ App/app/src/main/java/com/ufpe/if710/quentinhas/provider/MenuActivity.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.EditText
+import android.widget.TextView
 import android.widget.LinearLayout
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -14,7 +14,6 @@ import com.ufpe.if710.quentinhas.R
 import com.ufpe.if710.quentinhas.model.Menu
 import com.ufpe.if710.quentinhas.provider.MenusAdapter.Companion.MENU_ID
 import kotlinx.android.synthetic.main.activity_menu.*
-import kotlinx.android.synthetic.main.new_text_view.*
 
 class MenuActivity : AppCompatActivity() {
     private var menuID: String? = null
@@ -23,7 +22,11 @@ class MenuActivity : AppCompatActivity() {
     private var listProtein: ArrayList<String> = arrayListOf()
     private var listSide: ArrayList<String> = arrayListOf()
     private var listSize: ArrayList<String> = arrayListOf()
+
     private var parentLinearLayout: LinearLayout? = null
+    private var listTextViewProtein: ArrayList<TextView> = arrayListOf()
+    private var listTextViewSide: ArrayList<TextView> = arrayListOf()
+    private var listTextViewSize: ArrayList<TextView> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,27 +46,37 @@ class MenuActivity : AppCompatActivity() {
 
         for (protein in menu!!.protein){
             parentLinearLayout = findViewById(R.id.linear_layout_protein_list)
-            createTextView()
-            text_view.text = protein
+            listTextViewProtein.add(createTextView())
         }
 
         for (side in menu!!.side){
             parentLinearLayout = findViewById(R.id.linear_layout_side_list)
-            createTextView()
-            text_view.text = side
+            listTextViewSide.add(createTextView())
         }
 
         for (size in menu!!.size){
             parentLinearLayout = findViewById(R.id.linear_layout_size_list)
-            createTextView()
-            text_view.text = size
+            listTextViewSize.add(createTextView())
+        }
+
+        for (i in listTextViewProtein.indices){
+            listTextViewProtein[i].text = menu!!.protein[i]
+        }
+
+        for (i in listTextViewSide.indices){
+            listTextViewSide[i].text = menu!!.side[i]
+        }
+
+        for (i in listTextViewSize.indices){
+            listTextViewSize[i].text = menu!!.size[i]
         }
     }
 
-    private fun createTextView(){
+    private fun createTextView(): TextView{
         val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val rowView = inflater.inflate(R.layout.new_text_view, null)
         parentLinearLayout!!.addView(rowView, parentLinearLayout!!.childCount)
+        return rowView.findViewById(R.id.text_view)
     }
 
     private fun findMenu(){
