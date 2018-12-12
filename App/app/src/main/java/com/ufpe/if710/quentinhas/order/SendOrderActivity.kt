7a15c9a76d396a2cfc6b9c33a7b200587858199d
lmmc2 <lmmc2@cin.ufpe.br>
2018-12-12 17:42:20 -0300

    Navegação para tela de confirmação de pedido

--- App/app/src/main/java/com/ufpe/if710/quentinhas/order/SendOrderActivity.kt
+++ App/app/src/main/java/com/ufpe/if710/quentinhas/order/SendOrderActivity.kt
@@ -2,6 +2,7 @@ package com.ufpe.if710.quentinhas.order
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.ufpe.if710.quentinhas.R
 
 class SendOrderActivity : AppCompatActivity() {
 
