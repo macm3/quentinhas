19d5fbbd5f4471f19f8b90a67c26168493a44dfd
lmmc2 <lmmc2@cin.ufpe.br>
2018-12-11 22:24:39 -0300

    Deletando myordersactivity

--- App/app/src/main/java/com/ufpe/if710/quentinhas/OrderActivity.kt
+++ App/app/src/main/java/com/ufpe/if710/quentinhas/OrderActivity.kt
@@ -1,10 +1,24 @@
 package com.ufpe.if710.quentinhas
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.firebase.database.DatabaseReference
+import com.ufpe.if710.quentinhas.model.Menu
+import com.ufpe.if710.quentinhas.model.Order
 import kotlinx.android.synthetic.main.activity_order.*
+import java.util.ArrayList
 
 class OrderActivity : AppCompatActivity() {
+    private var orderID: String? = null
+    private var order: Order? = null
+    private var ordersRef: DatabaseReference? = null
+
+    private var parentLinearLayout: LinearLayout? = null
+    private var listTextViewSide: ArrayList<TextView> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +33,13 @@ class OrderActivity : AppCompatActivity() {
     override fun onSupportNavigateUp():Boolean {
         onBackPressed()
         return true
+    }
+
+    private fun createTextView(): TextView {
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val rowView = inflater.inflate(R.layout.new_text_view, null)
+        parentLinearLayout!!.addView(rowView, parentLinearLayout!!.childCount)
+        return rowView.findViewById(R.id.text_view)
     }
 
 }
--- App/app/src/main/java/com/ufpe/if710/quentinhas/provider/ProviderRegisterActivity.kt
+++ App/app/src/main/java/com/ufpe/if710/quentinhas/provider/ProviderRegisterActivity.kt
@@ -9,7 +9,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.ufpe.if710.quentinhas.MyOrdersActivity
 import com.ufpe.if710.quentinhas.R
 import com.ufpe.if710.quentinhas.model.User
 import kotlinx.android.synthetic.main.activity_provider_register.*
