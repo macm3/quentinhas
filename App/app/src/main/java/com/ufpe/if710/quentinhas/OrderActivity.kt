eb34901508aeec6e0b892e6d0c6ebed77b1a1515
lmmc2 <lmmc2@cin.ufpe.br>
2018-12-13 15:45:49 -0300

    Visualização de pedido com recycler

--- App/app/src/main/java/com/ufpe/if710/quentinhas/OrderActivity.kt
+++ App/app/src/main/java/com/ufpe/if710/quentinhas/OrderActivity.kt
@@ -1,24 +1,20 @@
 package com.ufpe.if710.quentinhas
 
-import android.content.Context
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.support.v7.app.AppCompatActivity
 import com.google.firebase.database.*
 import com.ufpe.if710.quentinhas.OrderAdapter.Companion.ORDER_ID
 import com.ufpe.if710.quentinhas.model.Order
+import com.ufpe.if710.quentinhas.order.ShowMenuAdapter
 import kotlinx.android.synthetic.main.activity_order.*
-import java.util.ArrayList
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.IOException
 
 class OrderActivity : AppCompatActivity() {
     private var orderID: String? = null
     private var order: Order? = null
     private var ordersRef: DatabaseReference? = null
-
-    private var parentLinearLayout: LinearLayout? = null
-    private var listTextViewSide: ArrayList<TextView> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,24 +39,19 @@ class OrderActivity : AppCompatActivity() {
     private fun updateUI(){
         protein_order.text = order!!.protein
 
-        for (side in order!!.side){
-            parentLinearLayout = findViewById(R.id.linear_layout_side_order_list)
-            listTextViewSide.add(createTextView())
+        try {
+            doAsync {
+                val adapterSides = ShowMenuAdapter(order!!.side)
+                uiThread {
+                    recycler_view_sides_order.adapter = adapterSides
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
 
         size_order.text = order!!.size
 
-        for (i in listTextViewSide.indices){
-            listTextViewSide[i].text = order!!.side[i]
-        }
-
-    }
-
-    private fun createTextView(): TextView {
-        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val rowView = inflater.inflate(R.layout.new_text_view, null)
-        parentLinearLayout!!.addView(rowView, parentLinearLayout!!.childCount)
-        return rowView.findViewById(R.id.text_view)
     }
 
     private fun findOrder(){
@@ -72,7 +63,6 @@ class OrderActivity : AppCompatActivity() {
             }
 
             override fun onDataChange(snapshot: DataSnapshot) {
-                listTextViewSide.clear()
                 if(snapshot.exists()){
                     val data = snapshot.children.first()
                     order = data.getValue(Order::class.java)
